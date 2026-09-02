465d5a888d8b48ca472b0f7b5df48a2455e0a4ea
Vishnu R <vishnu6364748848@gmail.com>
2026-09-02 23:21:30 +0530

    created camera

--- src/main.js
+++ src/main.js
@@ -4,3 +4,9 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+); // field of view, aspect ratio, near plane, far plane
