e0f645fe7328de3e51af78041187a555527915ff
Vishnu R <vishnu6364748848@gmail.com>
2026-09-07 16:53:31 +0530

    refactor: replace orthographic camera with perspective camera and update cube material to MeshNormalMaterial

--- src/main.js
+++ src/main.js
@@ -10,22 +10,17 @@ const size = {
   height: window.innerHeight,
 };
 
-const camera = new THREE.OrthographicCamera(
-  size.width / -2,
-  size.width / 2,
-  size.height / 2,
-  size.height / -2,
-  1,
+const camera = new THREE.PerspectiveCamera(
+  75,
+  size.width / size.height,
+  0.1,
   1000,
 ); // field of view, aspect ratio, near plane, far plane
 
 const time = new THREE.Clock();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1); // width, height, depth
-const material = new THREE.MeshBasicMaterial({
-  color: "red",
-  wireframe: true,
-});
+const material = new THREE.MeshNormalMaterial();
 const cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
@@ -55,9 +50,6 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
   renderer.setSize(size.width, size.height);
 });
-
-camera.zoom = 114;
-camera.updateProjectionMatrix();
 
 const animate = () => {
   const delta = time.getElapsedTime();
