7a0df21657cf86eb5490f9937eab0b45d5627a43
Vishnu R <vishnu6364748848@gmail.com>
2026-09-11 20:46:40 +0530

    refactor: remove OrbitControls and animation loop, update camera settings, and switch to BasicMaterial

--- three-js/src/main.js
+++ three-js/src/main.js
@@ -1,70 +1,35 @@
 import "./style.css";
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-const scene = new THREE.Scene();
-
-// Give the scene a gray background so you can clearly tell the canvas is running
-scene.background = new THREE.Color("#1a1a1a");
-
-const size = {
+const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
 
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
-  size.width / size.height,
+  sizes.width / sizes.height,
   0.1,
-  1000,
+  100,
 );
-camera.position.set(2, 2, 4);
 
-// // 1. Lights that ensure it cannot be pitch black
-// const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
-// scene.add(ambientLight);
+camera.position.set(0, 0, 3);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-directionalLight.position.set(3, 4, 2);
-scene.add(directionalLight);
-
-const directionalLightHelper = new THREE.DirectionalLightHelper(
-  directionalLight,
-);
-scene.add(directionalLightHelper);
-
-// 2. Visible test cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({
-  color: "red", // Neon green so it's impossible to miss
-  roughness: 0.3,
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
 });
+
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 const canvas = document.querySelector("#webgl");
-const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-renderer.setSize(size.width, size.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-
-window.addEventListener("resize", () => {
-  size.width = window.innerWidth;
-  size.height = window.innerHeight;
-
-  camera.aspect = size.width / size.height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(size.width, size.height);
+const render = new THREE.WebGLRenderer({
+  canvas,
 });
 
-const animate = () => {
-  cube.rotation.y += 0.01;
-  cube.rotation.x += 0.005;
+render.setSize(sizes.width, sizes.height);
+render.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-  controls.update();
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-};
-animate();
+render.render(scene, camera);
