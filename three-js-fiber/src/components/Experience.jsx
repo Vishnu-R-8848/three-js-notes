68a979087a3052d3f7fe06f384deba8c0e917b07
Vishnu R <vishnu6364748848@gmail.com>
2026-09-16 00:30:48 +0530

    feat: load GLTF model and play death animation

--- three-js-fiber/src/components/Experience.jsx
+++ three-js-fiber/src/components/Experience.jsx
@@ -1,6 +1,6 @@
-import { Text } from '@react-three/drei';
+import { Text, useAnimations, useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const Experience = () => {
 
@@ -10,6 +10,14 @@ const Experience = () => {
   useFrame((state, delta) => {
     meshRef.current.rotation.y += delta * 1;
   })
+
+  const { scene, animations } = useGLTF("./model.glb");
+  const { actions } = useAnimations(animations, scene);
+
+  useEffect(() => {
+    console.log(actions)
+    actions.Death.play()
+  }, [actions])
 
   return (
     <>
@@ -24,14 +32,16 @@ const Experience = () => {
       // position={[-3.5, 0.2, 1]}
       // scale={0.2}
       >
-        <boxGeometry args={[1, 1, 1]} />
+        {/* <boxGeometry args={[1, 1, 1]} /> */}
         {/* <circleGeometry /> */}
-        <meshStandardMaterial ref={materialRef} color={"red"} metalness={1} roughness={0.1} />
+        {/* <meshStandardMaterial ref={materialRef} color={"red"} metalness={1} roughness={0.1} /> */}
       </mesh>
 
       {/* <Text >
         Vishnu Naik Chouhan
       </Text> */}
+
+      <primitive object={scene} scale={0.2} position={[0, 0, 0]} />
     </>
   )
 }
